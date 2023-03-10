b75de952602e071bf318654d0adcbb028ba7a953
Lena EUGENE <lenaeugene@gmail.com>
2023-03-10 16:20:37 +0100

    Added prettier and astro-image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,12 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		variants: {
+			backgroundColor: ["responsive", "hover", "focus", "active"],
+			extend: {
+				scale: ["group-hover"],
+			},
+		},
 	},
 	plugins: [],
 }
